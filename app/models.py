4343f0501e13781ecf8c7c4219a18c463f039f00
SungHun Kim <kimtiger1201@naver.com>
2022-07-25 02:22:11 +0900

    added app/index view

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# 데이터 저장의 형태를 설정
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
